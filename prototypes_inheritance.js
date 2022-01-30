f5d30711eab8c7cca0216b0ae50865d452933fab
Vadzim Aleshchyk <vadzim_aleshchyk@epam.com>
2022-01-30 20:32:21 +0300

    change myNew function

--- prototypes_inheritance.js
+++ prototypes_inheritance.js
@@ -1,8 +1,12 @@
 function myNew(constructor, ...args) {
-    return new constructor(...args);
+    const obj = {};
+    obj.__proto__ = constructor.prototype;
+    obj.name = args[0];
+    obj.age = args[1];
+    return obj;
 }
 
-function Person	(name,	age)	{
+function Person	(name, age)	{
     this.name = name;
     this.age = age;
 }
@@ -15,7 +19,8 @@ const jack	= new Person('Jack',	40);
 console.log(john1.introduce());	//	My name is	John and	I am 30
 console.log(jack.introduce());	//	My name is	Jack and	I am 40
 
-const john = myNew(Person,'John', 30);
+const john = myNew(Person,'Sasha', 99);
+console.log(john)
 console.log(john.introduce())
 const j = new Person('John',	30);
 console.log(j)
